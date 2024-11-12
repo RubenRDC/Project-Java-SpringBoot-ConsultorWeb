e62dceb911947c2820aa2467e31aaaa79e7eb617
Ruben <rubenrdc2003@gmail.com>
2024-11-12 19:31:21 -0300

    + Se agrega metodo de control de excepcion SQL, de peticion URL mal formada, de JSON mal formado y mensaje personalizado en la validacion de Objetos.

--- src/main/java/com/rubenrdc/consultArtWeb/controllers/GlobalExceptionController.java
+++ src/main/java/com/rubenrdc/consultArtWeb/controllers/GlobalExceptionController.java
@@ -1,11 +1,16 @@
 package com.rubenrdc.consultArtWeb.controllers;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  *
@@ -15,12 +20,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> ErrorValidacionDeObjetoRecibido(/*HttpServletRequest request, Model model, MethodArgumentNotValidException ex*/) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<?> ErrorValidacionDeObjetoRecibido(/*HttpServletRequest request, Model model,*/ MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+        Map errorMap = new HashMap();
+        for (FieldError fieldError : fieldErrors) {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errorMap);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<?> ErrorJsonMalFormado() {
-        return ResponseEntity.badRequest().body(Map.of("error","JSON Mal Formado."));
+        return ResponseEntity.badRequest().body(Map.of("error", "JSON Mal Formado."));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> MalFormadoPathVariable() {
+        return ResponseEntity.badRequest().body(Map.of("error", "Peticion Mal formada."));
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<?> ValorUnicoYaExistenteEnDB(SQLIntegrityConstraintViolationException ex) {
+        return ResponseEntity.badRequest().body(Map.of("error", ex.getMessage()));
     }
 }
