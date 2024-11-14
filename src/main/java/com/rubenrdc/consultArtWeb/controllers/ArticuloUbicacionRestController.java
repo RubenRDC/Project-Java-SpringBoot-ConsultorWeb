f9bc7c03ba848d4ef7f77a5609fc9603badb6676
Ruben <rubenrdc2003@gmail.com>
2024-11-14 18:50:20 -0300

    + Se crea 'ArticuloUbicacionRestController' con la url 'api/articulos/{codigo}/ubicaciones' como base

---
+++ src/main/java/com/rubenrdc/consultArtWeb/controllers/ArticuloUbicacionRestController.java
@@ -0,0 +1,14 @@
+package com.rubenrdc.consultArtWeb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Ruben
+ */
+@RestController
+@RequestMapping(path = "api/articulos/{codigo}/ubicaciones")
+public class ArticuloUbicacionRestController {
+    
+}
