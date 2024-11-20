edea0c8a093c07a9e590b435fd04b5baeaff2f4d
Ruben <rubenrdc2003@gmail.com>
2024-11-20 20:54:14 -0300

    + Se agrega visualizacion basica de informacion con Thymeleaf y clase @Controller.

--- src/main/java/com/rubenrdc/consultArtWeb/SpringSegurityConfig.java
+++ src/main/java/com/rubenrdc/consultArtWeb/SpringSegurityConfig.java
@@ -16,10 +16,10 @@ public class SpringSegurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests((authz) -> authz
-                .requestMatchers(HttpMethod.GET, "/api/articulos", "/api/articulos/{codigo}","api/depositos","api/depositos/{cod}","api/articulos/{codigo}/ubicaciones").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/articulos", "/api/articulos/{codigo}","api/depositos","api/depositos/{cod}","api/articulos/{codigo}/ubicaciones").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/articulos", "/api/articulos/{codigo}","api/depositos","api/depositos/{cod}").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/articulos", "/api/articulos/{codigo}","api/depositos","api/depositos/{cod}","api/articulos/{codigo}/ubicaciones/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/articulos/{codigo}", "/api/articulos", "/api/articulos/{codigo}", "api/depositos", "api/depositos/{cod}", "api/articulos/{codigo}/ubicaciones").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/articulos", "/api/articulos/{codigo}", "api/depositos", "api/depositos/{cod}", "api/articulos/{codigo}/ubicaciones").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/api/articulos", "/api/articulos/{codigo}", "api/depositos", "api/depositos/{cod}").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/api/articulos", "/api/articulos/{codigo}", "api/depositos", "api/depositos/{cod}", "api/articulos/{codigo}/ubicaciones/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/login").permitAll()
                 //.requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                 //.requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
--- src/main/java/com/rubenrdc/consultArtWeb/controllers/ArticuloController.java
+++ src/main/java/com/rubenrdc/consultArtWeb/controllers/ArticuloController.java
@@ -1,38 +1,28 @@
 package com.rubenrdc.consultArtWeb.controllers;
 
-import com.rubenrdc.consultArtWeb.Dao.IArticuloDao;
-import com.rubenrdc.consultArtWeb.models.Articulo;
-import com.rubenrdc.consultArtWeb.models.ArticuloDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.rubenrdc.consultArtWeb.Dao.IArticuloUbicacionDao;
+import com.rubenrdc.consultArtWeb.services.ArticuloUbicacionService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author Ruben
  */
 @Controller
+@RequestMapping(path = "/articulos")
 public class ArticuloController {
 
     @Autowired
-    private IArticuloDao artDao;
-    @Autowired
-    private IArticuloUbicacionDao ubicDao;
+    private ArticuloUbicacionService artDao;
 
-    @GetMapping(path = "/articulos/{codigo}")
-    public String infoArticuloView(Model model, @PathVariable String codigo) {
+    @GetMapping(path = "/{codigo}")
+    public String infoArticuloView(Model model, @PathVariable(required = true) String codigo) {
         if (!codigo.isBlank()) {
-            Articulo findArticuloByCodigo = artDao.findArticuloByCodigo(codigo);
-            if (findArticuloByCodigo != null) {
-                findArticuloByCodigo.setListCantsFromUbics(ubicDao.findUbicacionByCodeArticulo(codigo));
-                ArticuloDTO artDTO = new ArticuloDTO(findArticuloByCodigo);
-                model.addAttribute("Articulo", artDTO);
-            } else {
-                return "/";
-            }
+            model.addAttribute("Articulo", artDao.findArticuloDTOCompleteByIdOrCode(codigo));
         }
         return "articulo";
     }
