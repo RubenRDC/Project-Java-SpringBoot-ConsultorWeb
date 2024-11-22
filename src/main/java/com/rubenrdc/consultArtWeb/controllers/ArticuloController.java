bc8dd8ae40d06643b7b59a7fbd147ba36ccba786
Ruben <rubenrdc2003@gmail.com>
2024-11-22 20:14:29 -0300

    + Se agrega el buscador por codigo y descripcion en la vista y su metodo de obtencion en la clase controladora.

--- src/main/java/com/rubenrdc/consultArtWeb/controllers/ArticuloController.java
+++ src/main/java/com/rubenrdc/consultArtWeb/controllers/ArticuloController.java
@@ -1,12 +1,15 @@
 package com.rubenrdc.consultArtWeb.controllers;
 
+import com.rubenrdc.consultArtWeb.models.ArticuloDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.rubenrdc.consultArtWeb.services.ArticuloUbicacionService;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -27,4 +30,19 @@ public class ArticuloController {
         return "articulo";
     }
 
+    @GetMapping()
+    public String getlistArticulos(Model model) {
+        model.addAttribute("articulos", artDao.findAllArticulosDTO());
+        return "listaArticulos";
+    }
+
+    @GetMapping(params = {"codigo", "descripcion"})
+    public String getArticulosLikeDescrip(Model model, @RequestParam(name = "descripcion", required = false, defaultValue = "") String descripcion, @RequestParam(name = "codigo", required = false, defaultValue = "") String codigo) {
+        List<ArticuloDTO> findAllArt = artDao.findArticuloDTOLikeDesc(codigo, descripcion);
+        model.addAttribute("articulos", findAllArt);
+        if (!findAllArt.isEmpty()) {
+            return "listaArticulos";
+        }
+        return "listaArticulos";
+    }
 }
